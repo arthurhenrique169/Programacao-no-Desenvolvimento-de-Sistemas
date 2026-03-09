664ba8a198b8c6ad2eb589cb4c728650bafd8604
arthurhenrique169 <oliveira.brito.arthur@escola.pr.gov.br>
2026-03-09 11:14:15 -0300

    aula09/03
    fd

---
+++ Aula 1 Instrução Atribuição.py
@@ -0,0 +1,12 @@
+# Variavel é um espaço de memoria que
+# Tem um nome e guarda algum dado do meu programa
+nome = "Arthur"
+sobrenome = "Henrique"
+numero = 1
+
+# Expressoes sao quaisquer combinações de
+# valor, variável e operadores
+resultado = numero + numero + 5
+resultado = (numero * (numero * numero) + 33)
+
+print(resultado)
